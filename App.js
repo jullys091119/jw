d91678ea973cbcbf2c61dc676f39dc4ff0c53750
Julian <informacionlinux271087@gmail.com>
2024-03-11 14:59:36 -0700

    Se fixio el problema de las fuentes quedando el proyecto sin nungun problema

--- App.js
+++ App.js
@@ -4,21 +4,9 @@ import { PaperProvider } from 'react-native-paper';
 import { ProviderModeBlack } from './context/context';
 import { ProviderQuestions } from './context/questionsProvider';
 
-import * as Font from 'expo-font';
+
 
 const App = () => {
-  useEffect(() => {
-    const loadFonts = async () => {
-      return Font.loadAsync({
-        'righteous': require('./assets/fonts/Righteous-Regular.ttf'),
-        'poiret': require('./assets/fonts/PoiretOne-Regular.ttf'),
-        'merri': require('./assets/fonts/MerriweatherSans-VariableFont_wght.ttf'),
-      });
-    };
-
-    loadFonts();
-  }, []);
-
   return (
     <ProviderModeBlack>
       <ProviderQuestions>
