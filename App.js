9d96d301a3ecdb5a276396ee18e1ffefd0092e64
Julian <informacionlinux271087@gmail.com>
2024-06-07 21:57:58 -0700

    Fix a la pantalla de la atalaya

--- App.js
+++ App.js
@@ -4,9 +4,27 @@ import { PaperProvider } from 'react-native-paper';
 import { ProviderModeBlack } from './context/context';
 import { ProviderQuestions } from './context/questionsProvider';
 import * as Font from 'expo-font';
+import * as Updates from 'expo-updates';
 
 const App = () => {
   const [appIsReady, setAppIsReady] = useState(false);
+
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() here if needed for your purposes
+      console.log(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+  
+  useEffect(() => {
+    onFetchUpdateAsync()
+  }, []);
 
   useEffect(() => {
       const prepare = async () => {
@@ -21,7 +39,7 @@ const App = () => {
           };
   
           await loadFonts();
-          await new Promise((resolve) => setTimeout(resolve, 5000)); // Espera 5 segundos
+          await new Promise((resolve) => setTimeout(resolve, 10000)); // Espera 5 segundos
         } catch (e) {
           console.warn(e);
         } finally {
--- screens/WatchTower.js
+++ screens/WatchTower.js
@@ -1,59 +1,53 @@
-import React, { useEffect, useState } from "react"
-import { View, Text, StyleSheet, ScrollView } from "react-native"
-import { watchTowerTopics } from "../firebase"
-import Accordion from "../components/Acordion"
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { watchTowerTopics } from "../firebase";
+import Accordion from "../components/Acordion";
 
-const WatchTower =  () => {
-	const [data, setData] = useState([]);
+const WatchTower = () => {
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const dataObj = await watchTowerTopics();
-      const values = Object.values(dataObj)
-      setData(values)
+      const values = Object.values(dataObj);
+      setData(values);
     };
     fetchData();
   }, []);
 
-	return (
-		<View>
-      <View style={styles.containerTitle}>
-        <Text style={styles.title}>Respuesta Revista Atalaya</Text>
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>Respuesta Revista Atalaya</Text>
+        </View>
+        {data.map((question, index) => (
+          <Accordion
+            key={index}
+            questions={question.pregunta}
+            answers={question.respuesta}
+          />
+        ))}
       </View>
-			{
-				data.map((question, index) => {
-        return(
-          <View style={styles.container} key={index}>
-           <ScrollView>
-              <Accordion
-                questions={question.pregunta}
-                answers={question.respuesta}
-                key={index}
-              />
-            </ScrollView>
-          </View>
-        )
-				})
-			}
-		</View>
-	);
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 10,
+    paddingBottom: 20, // Padding to prevent content from sticking to the bottom
+  },
+  containerTitle: {
+    marginTop: 50,
+    marginBottom: 20, // Add some bottom margin for better spacing
+  },
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+    paddingVertical: 18,
+  },
+});
 
-containerAcordion: {
- borderWidth: 2
-},
-
-containerTitle: {
- marginTop: 50,
-},
-
-title: {
- fontSize: 25,
- textAlign:"center",
- paddingVertical: 18
-},
-})
-
-export default WatchTower
+export default WatchTower;
